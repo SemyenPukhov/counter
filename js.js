591b0d09d570b3aedb735412153d7ad84987818e
SemyenPukhov <sspukhov@yandex.ru>
2018-09-24 21:53:51 +0300

    message

---
+++ js.js
@@ -0,0 +1,35 @@
+var total = document.getElementById("total");
+var plus = document.getElementById("plus");
+var minus = document.getElementById("minus");
+
+plus.addEventListener("click", function() {
+    writeResult("plus");
+    setAnimation(plus);
+});
+
+minus.addEventListener("click", function() {
+    writeResult("minus");
+    setAnimation(minus);
+});
+
+function writeResult(type) {
+    if (type == "plus") {
+        if (total.textContent == 0) {
+            total.textContent = 1;
+            return;
+        }
+        total.textContent = +total.textContent + 1;
+    }
+    if(type == "minus") {
+        if (total.textContent == 0) {
+            total.textContent = -1;
+            return;
+        }
+        total.textContent = +total.textContent - 1;
+    }
+}
+
+function setAnimation(node) {
+    var img = node.firstChild.style.animationName = "pulse";
+    setTimeout(function(){node.firstChild.style.animationName = "none";}, 1500);
+}
